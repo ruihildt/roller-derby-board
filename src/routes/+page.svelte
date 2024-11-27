6ca7739922e8f9a0f347618ee5a8c28f6f582951
rui hildt <rui@mullvad.net>
2024-11-27 13:10:45 +0100

    Center canvas in the viewport

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,15 +19,15 @@
 	}
 </script>
 
-<main class="min-h-screen bg-gray-100">
-	<div class="container mx-auto px-4 py-8">
-		<div class="mx-auto aspect-[59.8/42] max-h-screen w-full max-w-[1200px]">
-			<BoardCanvas bind:highResCanvas recordingComplete={handleRecordingComplete} />
-		</div>
-		{#if showPreview && recordedBlob}
-			<VideoPreview videoBlob={recordedBlob} close={handlePreviewClose} />
-		{/if}
+<main class="flex min-h-screen items-center justify-center bg-gray-100">
+	<div class="mx-auto aspect-[59.8/42] max-h-screen w-full max-w-[1200px]">
+		<BoardCanvas bind:highResCanvas recordingComplete={handleRecordingComplete} />
 	</div>
-	<Toolbar {highResCanvas} recordingComplete={handleRecordingComplete} />
-	<AboutPage />
 </main>
+
+{#if showPreview && recordedBlob}
+	<VideoPreview videoBlob={recordedBlob} close={handlePreviewClose} />
+{/if}
+
+<Toolbar {highResCanvas} recordingComplete={handleRecordingComplete} />
+<AboutPage />
