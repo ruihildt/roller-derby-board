aefc1a04402e3a757072d9f2e5666ee5e3527220
rui hildt <rui@mullvad.net>
2024-11-29 08:55:36 +0100

    Optimize css build size

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { purgeCss } from 'vite-plugin-tailwind-purgecss';
+
 import pkg from './package.json';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), purgeCss()],
 	define: {
 		'import.meta.env.PACKAGE_VERSION': JSON.stringify(pkg.version),
 		'import.meta.env.BUILD_DATE': JSON.stringify(new Date().toISOString())
