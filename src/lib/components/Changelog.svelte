48f80576994bdd4af39267ccf8bc320e4974ba9a
rui hildt <rui@mullvad.net>
2024-11-29 15:35:20 +0100

    Move changelog to top

--- src/lib/components/Changelog.svelte
+++ src/lib/components/Changelog.svelte
@@ -6,7 +6,7 @@
 
 <button
 	on:click={() => (hidden = false)}
-	class="fixed bottom-4 right-4 rounded-lg bg-white px-4 py-2 text-sm text-gray-700 hover:bg-primary-200"
+	class="fixed right-4 top-4 rounded-lg bg-white px-4 py-2 text-sm text-gray-700 hover:bg-primary-200"
 >
 	Changelog
 </button>
