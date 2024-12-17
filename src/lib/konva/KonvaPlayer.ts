0b5ed4e0233d3f165ae51cf6dd3c3b39c499f191
rui hildt <rui@mullvad.net>
2024-12-17 22:42:29 +0100

    Refactor dragmove in separate methods

--- src/lib/konva/KonvaPlayer.ts
+++ src/lib/konva/KonvaPlayer.ts
@@ -22,8 +22,9 @@ interface PlayerCircleConfig {
 export class KonvaPlayer {
 	static readonly PLAYER_RADIUS = TRACK_SCALE / 2.4;
 	static readonly STROKE_WIDTH = TRACK_SCALE / 10;
-	protected trackGeometry: KonvaTrackGeometry;
+	static readonly PUSH_FORCE = 0.1;
 
+	protected trackGeometry: KonvaTrackGeometry;
 	group: Konva.Group;
 
 	constructor(x: number, y: number, layer: Konva.Layer, trackGeometry: KonvaTrackGeometry) {
@@ -50,49 +51,60 @@ export class KonvaPlayer {
 		const circle = new Konva.Circle(circleConfig);
 		this.group.add(circle);
 
-		this.group.on('dragmove', () => {
-			const players = layer
-				.find('Group')
-				.filter((node) => node instanceof Konva.Group) as Konva.Group[];
-			const currentGroup = this.group;
-
-			players.forEach((otherGroup) => {
-				if (otherGroup === currentGroup) return;
-
-				const dx = otherGroup.x() - currentGroup.x();
-				const dy = otherGroup.y() - currentGroup.y();
-				const distance = Math.sqrt(dx * dx + dy * dy);
-
-				const totalRadius = KonvaPlayer.PLAYER_RADIUS * 2 + KonvaPlayer.STROKE_WIDTH;
-
-				if (distance < totalRadius) {
-					const dirX = dx / distance;
-					const dirY = dy / distance;
-					const pushForce = 0.1;
-
-					otherGroup.x(currentGroup.x() + dirX * (totalRadius + pushForce));
-					otherGroup.y(currentGroup.y() + dirY * (totalRadius + pushForce));
-
-					otherGroup.fire('dragmove');
-				}
-			});
-
-			if (this instanceof KonvaTeamPlayer) {
-				this.updateInBounds(this.trackGeometry);
-			}
-
-			layer.batchDraw();
-		});
+		this.group.on('dragmove', () => this.handleDragMove(layer));
 
 		layer.add(this.group);
 		layer.batchDraw();
+	}
+
+	protected handleDragMove(layer: Konva.Layer): void {
+		this.handleCollisions(layer);
+
+		if (this instanceof KonvaTeamPlayer) {
+			this.updateInBounds(this.trackGeometry);
+		}
+
+		layer.batchDraw();
+	}
+
+	protected handleCollisions(layer: Konva.Layer): void {
+		const players = this.getOtherPlayers(layer);
+		players.forEach((player) => this.checkAndResolveCollision(player));
+	}
+
+	protected getOtherPlayers(layer: Konva.Layer): KonvaPlayer[] {
+		return layer
+			.find('Group')
+			.filter((node) => node instanceof Konva.Group && node !== this.group)
+			.map((group) => (group as Konva.Group).getAttr('player')) as KonvaPlayer[];
+	}
+
+	protected checkAndResolveCollision(otherPlayer: KonvaPlayer): void {
+		const distance = this.distanceToPlayer(otherPlayer);
+		const totalRadius = KonvaPlayer.PLAYER_RADIUS * 2 + KonvaPlayer.STROKE_WIDTH;
+
+		if (distance < totalRadius) {
+			this.pushOtherPlayer(otherPlayer, distance, totalRadius);
+		}
+	}
+
+	protected pushOtherPlayer(otherPlayer: KonvaPlayer, distance: number, totalRadius: number): void {
+		const dx = otherPlayer.group.x() - this.group.x();
+		const dy = otherPlayer.group.y() - this.group.y();
+		const dirX = dx / distance;
+		const dirY = dy / distance;
+
+		otherPlayer.group.x(this.group.x() + dirX * (totalRadius + KonvaPlayer.PUSH_FORCE));
+		otherPlayer.group.y(this.group.y() + dirY * (totalRadius + KonvaPlayer.PUSH_FORCE));
+
+		otherPlayer.group.fire('dragmove');
 	}
 
 	protected getBaseCircle(): Konva.Circle {
 		return this.group.findOne('.baseCircle') as Konva.Circle;
 	}
 
-	distanceTo(other: KonvaPlayer): number {
+	distanceToPlayer(other: KonvaPlayer): number {
 		const dx = this.group.x() - other.group.x();
 		const dy = this.group.y() - other.group.y();
 		return Math.sqrt(dx * dx + dy * dy);
